512828ab8cd06b3e22f84404e750b4578c0e093c
why <hustwhy2000@foxmail.com>
2018-07-27 15:35:40 +0800

    modified why

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: "华中科技大学信息安全实验平台" });
+});
+router.get
+
+module.exports = router;
