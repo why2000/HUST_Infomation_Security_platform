fa20f96b67458ac1586c7ad937ba92674e912cf8
milkybird98 <milkybird98@outlook.com>
2018-09-08 19:20:34 +0800

    information change done

--- controllers/information_controller.js
+++ controllers/information_controller.js
@@ -1,21 +1,30 @@
+'use strict'
+
+let UserDB = require('../models/user_db');
 let UserValidator = require('../validators/user_validator');
+
 
 exports.getIndex = async (req, res, next) => {
   if (!req.session.loginUser) {
     res.redirect('/');
   } else {
-    res.render('information', { student_name:await UserValidator.getUserTypeById(req.session.loginUser) });
+    res.render('information', { student_name: await UserValidator.getUserTypeById(req.session.loginUser) });
   }
 };
 
 exports.setData = async (req, res, next) => {
-  let name = req.body.name;
-  let col = req.body.col;
-  let uid = req.body.uid;
-  let pw = req.body.pw;
-  console.log(name);
-  console.log(col);
-  console.log(uid);
-  console.log(pw);
-  res.status(200);
+  if (req.session.loginUser) {
+
+    let data = {
+      uid: req.body.uid,
+      pwd: req.body.pw,
+      name: req.body.name
+    }
+
+    if (await UserDB.changeUserData(data)) {
+      console.log("222");
+      res.status(200).send("change successfully");
+    }
+  }
+  else { res.status(500); }
 }
--- models/user_db.js
+++ models/user_db.js
@@ -8,6 +8,7 @@ let MongoDB = require('mongodb');
 let MongoClient = MongoDB.MongoClient;
 let IsEmpty = require('is-empty');
 
+
 let db;
 MongoClient.connect(ConfigSet.DATABASE_URL, (err, client) => {
     if (err) {
@@ -15,7 +16,7 @@ MongoClient.connect(ConfigSet.DATABASE_URL, (err, client) => {
         throw err;
     } else {
         db = client.db(ConfigSet.DATABASE_NAME);
-        db.createCollection(ConfigSet.COLLECTION_NAME, function(err, res) {
+        db.createCollection(ConfigSet.COLLECTION_NAME, function (err, res) {
             if (err) {
                 UserLogger.error(`database error => ${err.stack}`);
                 throw err;
@@ -23,27 +24,38 @@ MongoClient.connect(ConfigSet.DATABASE_URL, (err, client) => {
                 //console.log("Successfully creat col");
                 ;
             }
-          });
+        });
     }
 })
 
-const findUserByName = async function(username){
+const changeUserData = async function (data) {
     var user = db.collection('user');
-    var doc = user.find({type: 'user-info', username: username});
-    if(!doc){
+    await user.update({ userid: data.uid }, { $set: { username: data.name, password: data.pwd } }, function (err, res) {
+        if (err) {
+            UserLogger.error(`database error => ${err.stack}`);
+            throw err
+        }
+    });
+    return true;
+}
+
+const findUserByName = async function (username) {
+    var user = db.collection('user');
+    var doc = user.find({ type: 'user-info', username: username });
+    if (!doc) {
         return false;
     }
-    else{
+    else {
         return doc;
     }
 }
 
-const findUserById = async function(userid){
+const findUserById = async function (userid) {
     var user = db.collection('user');
     console.log(userid);
-    try{
-        var result = await user.findOne({type: 'user-info', userid: userid});
-    }catch(err){
+    try {
+        var result = await user.findOne({ type: 'user-info', userid: userid });
+    } catch (err) {
         UserLogger.error(`database error => ${err.stack}`);
         throw err
     }
@@ -52,5 +64,6 @@ const findUserById = async function(userid){
 
 module.exports = {
     findUserByName,
-    findUserById
+    findUserById,
+    changeUserData
 }
